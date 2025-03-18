e32fd88eb7a17c65440ee9eb63312a6fd3e1ceea
Devansh5998 <devanshchalmani123@gmail.com>
2025-03-18 12:51:13 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Sum 
+{
+    public static void main(String[] args)
+    {
+
+        // Creating Scanner class object
+        Scanner scn = new Scanner(System.in);
+
+        // Enter first input
+        System.out.print("Enter First Number: ");
+        int a = scn.nextInt();
+
+        System.out.print("Enter Second Number: ");
+        int b = scn.nextInt();
+
+        System.out.println("Sum: " + (a + b));
+
+        // Closing the scanner to release resources
+        scn.close();
+    }
+}
+
