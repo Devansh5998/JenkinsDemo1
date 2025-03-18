bbea682a288f49b9a4e53cb37bba48614e167205
Devansh5998 <devanshchalmani123@gmail.com>
2025-03-18 12:59:05 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Calculate the sum
+sum_result = num1 + num2
+
+# Display the result
+print("The sum of", num1, "and", num2, "is:", sum_result)
+
